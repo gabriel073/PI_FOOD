88ad90edeaa869ad27e097a1a61a63db5c9a6c1c
´Gabriel <Gabrielmarzioli@hotmail.com>
2022-10-06 17:45:48 -0300

    implementando cors

--- api/src/app.js
+++ api/src/app.js
@@ -3,6 +3,7 @@ const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const routes = require('./routes/index.js');
+const cors = require('cors');
 
 require('./db.js');
 
@@ -10,7 +11,7 @@ require('./db.js');
 const server = express();
 
 server.name = 'API';
-
+server.use(cors());
 server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 server.use(bodyParser.json({ limit: '50mb' }));
 server.use(cookieParser());
