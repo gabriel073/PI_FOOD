1a5ed9e17af7ee3864dff234c02b180193b03db4
´Gabriel <Gabrielmarzioli@hotmail.com>
2023-01-14 12:40:13 -0300

    modificando variable dbport

--- api/src/db.js
+++ api/src/db.js
@@ -3,37 +3,36 @@ const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
 const {
-  DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, DB_PORT} = process.env;
+  DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, DB_PORT } = process.env;
 
-  let sequelize =
-    process.env.NODE_ENV === "production"
-   ? new Sequelize({
-       database: DB_NAME,
-       dialect: "postgres",
-       host: DB_HOST,
-       port: 5432,
-       username: DB_USER,
-       password: DB_PASSWORD, 
-       dbport: DB_PORT,
-       pool: {
-         max: 3,
-         Min: 1,
-         idle: 10000,
-    },
-       dialectOptions: {
-         ssl: {
-         require: true,
-           // Ref .: https://github.com/brianc/node postgres/issues/2009
-           rejectUnauthorized: false,
-         },
-         keepAlive: true,
+let sequelize =
+  process.env.NODE_ENV === "production"
+    ? new Sequelize({
+      database: DB_NAME,
+      dialect: "postgres",
+      host: DB_HOST,
+      port: DB_PORT,
+      username: DB_USER,
+      password: DB_PASSWORD,
+      pool: {
+        max: 3,
+        Min: 1,
+        idle: 10000,
+      },
+      dialectOptions: {
+        ssl: {
+          require: true,
+          // Ref .: https://github.com/brianc/node postgres/issues/2009
+          rejectUnauthorized: false,
         },
-        ssl: true,
-        })
-        : new Sequelize(
-          `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/food`, 
-          {  logging: false,  native: false }
-        );
+        keepAlive: true,
+      },
+      ssl: true,
+    })
+    : new Sequelize(
+      `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/food`,
+      { logging: false, native: false }
+    );
 //   logging: false, // set to console.log to see the raw SQL queries
 //   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 // });
@@ -65,8 +64,8 @@ const { Recipe, Diet } = sequelize.models;
 // 'carnicor , vegetarino , omnivora  , pesquero'
 //* muchos -> muchos 
 
-Recipe.belongsToMany(Diet,{through:'type_recipe'}) 
-Diet.belongsToMany(Recipe,{through:'type_recipe'}) 
+Recipe.belongsToMany(Diet, { through: 'type_recipe' })
+Diet.belongsToMany(Recipe, { through: 'type_recipe' })
 
 //* uno -> muchos
 // type.hasMany(recipe)
