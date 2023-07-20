10910c07d67f1c874b865de174f86d46c11498f1
gabriel073 <gabrielmarzioli@hotmail.com>
2023-07-20 17:19:25 -0300

    modificando url deploy BE

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,25 @@
 import './App.css';
 import React from 'react';
-import {BrowserRouter, Route} from "react-router-dom";
+import { BrowserRouter, Route } from "react-router-dom";
 import NavBar from "./components/navBar/NavBar.js";
 import Home from "./components/home/Home.js";
-import RecipeDetails  from './components/recipeDetails/RecipeDetails.js';
+import RecipeDetails from './components/recipeDetails/RecipeDetails.js';
 import NewRecipe from './components/newRecipe/NewRecipe';
 import axios from 'axios';
 import NotFound from './components/search/NotFound';
 
-axios.defaults.baseURL = 'https://pifood-production-26ed.up.railway.app';
+axios.defaults.baseURL = 'https://food-app.up.railway.app';
 // axios.defaults.baseURL =  'http://127.0.0.1:3001';                  
 
 function App() {
   return (
-<BrowserRouter>
-    <Route exact path="/" component={NavBar}></Route>
-    <Route exact path="/notFound" component={NotFound}></Route>
-    <Route exact path ="/recipes" component={Home}></Route>
-    <Route exact path ="/recipe" component={NewRecipe}></Route>
-    <Route exact path ="/recipes/:id" component={RecipeDetails}></Route>
-    </BrowserRouter>       
+    <BrowserRouter>
+      <Route exact path="/" component={NavBar}></Route>
+      <Route exact path="/notFound" component={NotFound}></Route>
+      <Route exact path="/recipes" component={Home}></Route>
+      <Route exact path="/recipe" component={NewRecipe}></Route>
+      <Route exact path="/recipes/:id" component={RecipeDetails}></Route>
+    </BrowserRouter>
   );
 }
 export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -3,21 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {Provider} from 'react-redux';
+import { Provider } from 'react-redux';
 import store from './store';
 import dotenv from "dotenv";
-
 
 dotenv.config();
 
 // axios.defaults.baseURL =  "http://54.175.191.76:3001" //|| "http://localhost:3001";
 // process.env.REACT_APP_API
 ReactDOM.render(
-   <Provider store={store}>
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-  </Provider>,  
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>,
   document.getElementById('root')
 );
 
